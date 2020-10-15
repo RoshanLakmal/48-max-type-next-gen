dcb5a4ed60f56cc33b019467abc43a19f73a5f9a
Roshan Elvitigala <roshanrox911@gmail.com>
2020-10-15 17:36:10 +1100

    let & const

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,21 @@
 const userName = 'Max';
 // userName = "Roshan";
 let age = 30;
+
+function add(a:number,b:number){
+    let result;
+    result = a+b;
+    return result;
+}
+
+// if(age>20){
+//     var isOld = true;
+// }
+
+// console.log(isOld); // works in the browser
+
+// if(age>20){
+//     let isOld = true; // Give eroor in typescript
+// }
+
+// console.log(isOld); // Will not work in the browser
